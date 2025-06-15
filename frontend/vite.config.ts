472e893c445a5eab9d3275a797d07a37045e745d
soulgamer00 <soulgamer00xx@gmail.com>
2025-06-15 10:09:06 +0700

    rail deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,32 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelteTesting } from '@testing-library/svelte/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    strictPort: false
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    strictPort: false
-  }
-})
+	plugins: [sveltekit()],
+	test: {
+		workspace: [
+			{
+				extends: './vite.config.ts',
+				plugins: [svelteTesting()],
+				test: {
+					name: 'client',
+					environment: 'jsdom',
+					clearMocks: true,
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts']
+				}
+			},
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
+});
